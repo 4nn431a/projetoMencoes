135b7b483b180773c85c80d3a0e37e4833e8afe9
4nn431a <annabeatriz.11santos12@gmail.com>
2023-05-26 21:24:42 +0900

    atualizações

--- www/js/java.js
+++ www/js/java.js
@@ -1,6 +1,7 @@
 var nome
 var selecao
 var selecaodisciplina
+var selecaomencao
 
 function cadastrar() {
     nome = document.getElementById('inputnome').value;
@@ -8,4 +9,22 @@ function cadastrar() {
 
     selecao = document.getElementById('disciplina')
     selecaodisciplina = selecao.options[selecao.selectIndex].value
+
+    selecao - document.getElementById('mencao')
+    selecaomencao = selecao.options[selecao.selectIndex].value
+
+    localStorage.setItem(selecaodisciplina,selecaomencao)
+}
+
+function listar() {
+    var exibirdisciplina
+    var exibirmencao
+
+    selecao = document.getElementById('disciplina')
+    selecaodisciplina = selecao.options[selecao.selectIndex].value
+
+    localStorage.setItem(selecaodisciplina)
+
+    document.getElementById('exibirdisciplina').textContent = selecaodisciplina
+    document.getElementById('exibirmencao').textContent = exibirmencao
 }
